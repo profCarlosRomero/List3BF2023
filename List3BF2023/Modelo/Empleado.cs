77aef923912ca48f1928a61e7baceed20348723b
profCarlosRomero <prof.c.romero@gmail.com>
2023-07-05 17:51:43 -0300

    Se agregan todos los atributos y las propiedades

--- List3BF2023/Modelo/Empleado.cs
+++ List3BF2023/Modelo/Empleado.cs
@@ -8,7 +8,32 @@ namespace List3BF2023.Modelo
 {
     class Empleado
     {
+        //NSSSUPER	ND
         private string nss;
-        private string nombre;
+        private string nombrep;
+        //private string apellido;
+        private DateTime fechan;
+        //private string dir;
+        private char sexo;
+        private double salario;
+
+        public Empleado(string nss, string nombrep, DateTime fechan, char sexo, double salario)
+        {
+            this.nss = nss;
+            this.nombrep = nombrep;
+            this.fechan = fechan;
+            this.sexo = sexo;
+            this.salario = salario;
+        }
+
+        public string NSS { get { return this.nss; } }
+
+        public string Nombrep { get { return this.nombrep; } set { this.nombrep = value; } }
+
+        public DateTime FechaN { get { return this.fechan; } set { this.fechan = value; } }
+
+        public char Sexo { get { return this.sexo; } set { this.sexo = value; } }
+
+        public double Salario { get { return this.salario; } set { this.salario = value; } }
     }
 }
